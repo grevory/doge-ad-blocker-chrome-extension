e11176a5341b1be2a10b174b40b667589b77f923
Gregory Pike <greg@gregpike.ca>
2014-03-14 19:19:27 -0400

    Allow images for each size

--- extension/replace-ads.js
+++ extension/replace-ads.js
@@ -5,17 +5,38 @@ function replaceAds() {
   body = body [0];
   var allPossibleAds = body.querySelectorAll('iframe, img');
 
-  var dogeUrl = 'http://i.imgur.com/SAcQS1n.gif';
   var adSizes = [
-    {'name':'Leaderboard', 'width': 728, 'height': 90},
-    {'name':'Medium Box', 'width': 300, 'height': 250}
+    {
+      'name':'Leaderboard', 
+      'width': 728, 
+      'height': 90,
+      'images': [
+        'http://i.kinja-img.com/gawker-media/image/upload/t_k-bigpic/195no95aw7ue7png.png'
+      ]
+    },
+    {
+      'name':'Super Leaderboard', 
+      'width': 970, 
+      'height': 90,
+      'images': [
+        'http://i.kinja-img.com/gawker-media/image/upload/t_k-bigpic/195no95aw7ue7png.png'
+      ]
+    },
+    {
+      'name':'Medium Box', 
+      'width': 300, 
+      'height': 250,
+      'images': [
+        'http://i.imgur.com/lZw9kxK.gif',
+        'http://static1.businessinsider.com/image/52f10e55eab8eae06bc2ff0e/reddits-hilarious-new-ad-sales-pitch-deck-with-doge-and-nicholas-cage-is-what-all-ad-sales-pitch-decks-should-be-like.jpg'
+      ]
+    }
   ];
 
   Array.prototype.forEach.call(allPossibleAds, function(el, i){
-
     adSizes.forEach(function(adSize, i){
       if (el.offsetWidth == adSize.width && el.offsetHeight == adSize.height) {
-        el.outerHTML = '<div style="width:'+adSize.width+'px; height:'+adSize.height+'px; background: url('+dogeUrl+'); border: 1px solid #ddd"></div>';
+        el.outerHTML = '<div style="width:'+adSize.width+'px; height:'+adSize.height+'px; background: url('+adSize.images[Math.floor(Math.random() * adSize.images.length)]+'); no-repeat fixed; -webkit-background-size: cover; border: 1px solid #ddd"></div>';
       }
     });
   });
